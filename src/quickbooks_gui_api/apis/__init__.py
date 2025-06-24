5744f307d4d64779f9c7ea7fe961dc73864b70e8
Derek Banker <152438422+Derek-Banker@users.noreply.github.com>
2025-06-24 14:43:13 -0700

    Fix package imports in __init__ files and ensure newline

--- src/quickbooks_gui_api/apis/__init__.py
+++ src/quickbooks_gui_api/apis/__init__.py
@@ -1,9 +1,9 @@
 # src\quickbooks_gui_api\apis\__init__.py
 
-from src.quickbooks_gui_api.apis.invoices   import Invoices
-from src.quickbooks_gui_api.apis.reports    import Reports
+from .invoices import Invoices
+from .reports import Reports
 
-from src.quickbooks_gui_api.apis.api_exceptions import ConfigFileNotFound
+from .api_exceptions import ConfigFileNotFound
 
 __all__ = [
            "Invoices",
